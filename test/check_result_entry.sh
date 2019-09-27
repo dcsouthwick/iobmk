d49fa0de6eb47f68c18308f80e60c0b436c9f0b1
Domenico Giordano <domenico.giordano@cern.ch>
2019-09-27 16:57:54 +0000

    Resolve BMK-236

---
+++ test/check_result_entry.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Script to test in the CI that a specific benchmark produced the expected output
+
+BMKLIST=$1
+TESTFILE=$2
+
+function find_token(){
+    token=$1
+    echo -e "Looking for result entry for benchmark $bmk"
+    COUNTS=$(grep -c "$1" $TESTFILE)
+    if [ "$COUNTS" == "0" ]; then
+	echo -e "ERROR: token '$token' for benchmark $bmk not found in $TESTFILE"
+	exit 1
+    fi
+}
+
+echo -e "List of benchmarks to check $BMKLIST"
+echo -e "File to test $TESTFILE"
+
+echo -e "Dumping $TESTFILE"
+cat $TESTFILE
+echo ""
+
+for bmk in ${BMKLIST//;/ }
+do 
+    [ "$bmk" == "kv" ] && find_token "KV cpu performance \[evt/sec\]"
+    [ "$bmk" == "DB12" ] && find_token "DIRAC Benchmark ="
+    [ "$bmk" == "hs06_32" ] && find_token "HS06 32 bit Benchmark ="
+    [ "$bmk" == "hs06_64" ] && find_token "HS06 64 bit Benchmark ="
+    [ "$bmk" == "spec2017" ] && find_token "SPEC2017 64 bit Benchmark ="
+    [ "$bmk" == "hepscore" ] && find_token "HEPSCORE Benchmark ="
+done
+exit 0
