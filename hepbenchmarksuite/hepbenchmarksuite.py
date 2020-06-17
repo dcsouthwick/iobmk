b3e83b178c147e73a88ee0ad71f0c61d07cb0f8d
David Southwick <david.southwick@cern.ch>
2020-06-17 13:54:16 +0200

    test integrating hepscore

--- hepbenchmarksuite/hepbenchmarksuite.py
+++ hepbenchmarksuite/hepbenchmarksuite.py
@@ -9,105 +9,113 @@ import json
 import logging
 import time
 
-from  hepbenchmarksuite import db12
-from  hepbenchmarksuite import utils
+from hepbenchmarksuite import db12
+from hepbenchmarksuite import utils
 
 _log = logging.getLogger(__name__)
 
+
 class HepBenchmarkSuite(object):
-  """********************************************************
-                *** HEP-BENCHMARK-SUITE ***
-   *********************************************************"""
+    """********************************************************
+                  *** HEP-BENCHMARK-SUITE ***
+     *********************************************************"""
 
+    RESULT_FILES = {
+        'hs06_32': 'HS06/hs06_32_result.json',
+        'hs06_64': 'HS06/hs06_64_result.json',
+        'spec2017': 'SPEC2017/spec2017_result.json',
+        'hepscore': 'HEPSCORE/hepscore_result.json',
+        'db12': 'db12_result.json',
+    }
 
-  RESULT_FILES = {
-      'hs06_32'  : 'HS06/hs06_32_result.json',
-      'hs06_64'  : 'HS06/hs06_64_result.json',
-      'spec2017' : 'SPEC2017/spec2017_result.json',
-      'hepscore' : 'HEPSCORE/hepscore_result.json',
-      'db12'     : 'db12_result.json',
-  }
+    def __init__(self,  config=None):
+        """Initialize setup"""
+        self._bench_queue = config['global']['benchmarks']
+        self.selected_benchmarks = config['global']['benchmarks'].copy()
+        self._config = config['global']
+        self._config_full = config
+        self._extra = {}
 
-  def __init__(self,  config=None):
-    """Initialize setup"""
-    self._bench_queue        = config['global']['benchmarks']
-    self.selected_benchmarks = config['global']['benchmarks'].copy()
-    self._config             = config['global']
-    self._config_full        = config
-    self._extra              = {}
+    def start(self):
+        _log.info("Starting benchmark suite")
 
-  def start(self):
-    _log.info ("Starting benchmark suite")
+        self._extra['start_time'] = time.strftime(
+            '%Y-%m-%dT%H:%M:%SZ', time.gmtime())
 
-    self._extra['start_time'] = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime())
+        if self._preflight() == 0:
+            self.run()
 
-    if self._preflight() == 0:
-      self.run()
+    def _preflight(self):
+        _log.info("Running pre-flight checks")
+        # TODO: Check config before running
+        _log.info(" - Checking provided work dirs exist...")
+        os.makedirs(self._config['rundir'], exist_ok=True)
+        os.makedirs(self._config_full['hepspec06']
+                    ['hepspec_volume'], exist_ok=True)
+        #os.makedirs(self._config_full['hepscore_benchmark']['rundir'], exist_ok=True)
+        # OK returns 0
+        # NOK retuns 1
+        return 0
 
-  def _preflight(self):
-    _log.info ("Running pre-flight checks")
-    # TODO: Check config before running
-    _log.info (" - Checking provided work dirs exist...")
-    os.makedirs(self._config['rundir'], exist_ok=True)
-    os.makedirs(self._config_full['hepspec06']['hepspec_volume'], exist_ok=True)
-    # OK returns 0
-    # NOK retuns 1
-    return 0
+    def run(self):
 
-  def run(self):
+        # Check if there are still benchmarks to run
+        if len(self._bench_queue) == 0:
+            self._extra['end_time'] = time.strftime(
+                '%Y-%m-%dT%H:%M:%SZ', time.gmtime())
+            self.cleanup()
 
-    # Check if there are still benchmarks to run
-    if len(self._bench_queue) == 0:
-      self._extra['end_time'] = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime())
-      self.cleanup()
+        else:
+            bench2run = self.dequeue()
+            _log.info("Running benchmark: {}".format(bench2run))
+            _log.info("Benchmarks left to run: {}".format(self._bench_queue))
 
-    else:
-      bench2run = self.dequeue()
-      _log.info("Running benchmark: {}".format(bench2run))
-      _log.info("Benchmarks left to run: {}".format(self._bench_queue))
+            if bench2run == 'db12':
+                db12.run_db12(rundir=self._config['rundir'], cpu_num=2)
 
-      if bench2run == 'db12':
-        db12.run_db12(rundir=self._config['rundir'], cpu_num=2)
+            elif bench2run == 'hepscore':
+                utils.run_hepscore(conf=self._config_full, bench=bench2run)
 
-      elif bench2run in ['hs06_32', 'hs06_64', 'spec2017']:
-        utils.run_hepspec(conf=self._config_full, bench=bench2run)
+            elif bench2run in ['hs06_32', 'hs06_64', 'spec2017']:
+                utils.run_hepspec(conf=self._config_full, bench=bench2run)
 
-      self.check_lock()
+            self.check_lock()
 
-  def dequeue(self):
-      return self._bench_queue.pop(0)
+    def dequeue(self):
+        return self._bench_queue.pop(0)
 
-  def check_lock(self):
-    # TODO: Check lock files
-    # loop until lock is released from benchmark
-    # print(os.path.exists("bench.lock"))
-    # Release lock and resume benchmarks
-    self.run()
+    def check_lock(self):
+        # TODO: Check lock files
+        # loop until lock is released from benchmark
+        # print(os.path.exists("bench.lock"))
+        # Release lock and resume benchmarks
+        self.run()
 
-  def cleanup(self):
-    #
-    # TODO: Check logs
+    def cleanup(self):
+        #
+        # TODO: Check logs
 
-    # compile metadata
-    self._result = utils.prepare_metadata(self._config, self._extra)
-    self._result.update({'profiles': {}})
+        # compile metadata
+        self._result = utils.prepare_metadata(self._config, self._extra)
+        self._result.update({'profiles': {}})
 
-    # Get results from each benchmark
-    for bench in self.selected_benchmarks:
-      try:
-          result_path = os.path.join(self._config['rundir'], self.RESULT_FILES[bench])
-          with open(result_path, "r") as result_file:
-            _log.info("Reading result file: {}".format(result_path))
+        # Get results from each benchmark
+        for bench in self.selected_benchmarks:
+            try:
+                result_path = os.path.join(
+                    self._config['rundir'], self.RESULT_FILES[bench])
+                with open(result_path, "r") as result_file:
+                    _log.info("Reading result file: {}".format(result_path))
 
-            self._result['profiles'].update(json.loads(result_file.read()))
+                    self._result['profiles'].update(
+                        json.loads(result_file.read()))
 
-      except Exception as e:
-        _log.warning('Skipping {} because of {}'.format(bench,e))
+            except Exception as e:
+                _log.warning('Skipping {} because of {}'.format(bench, e))
 
-    # Save complete json report
-    with open(os.path.join(self._config['rundir'] , self._config['file']), 'w') as fo:
-      fo.write(json.dumps(self._result))
+        # Save complete json report
+        with open(os.path.join(self._config['rundir'], self._config['file']), 'w') as fo:
+            fo.write(json.dumps(self._result))
 
-
-  def results(self):
-    return self._result
+    def results(self):
+        return self._result
--- hepbenchmarksuite/send_queue.py
+++ hepbenchmarksuite/send_queue.py
@@ -11,23 +11,24 @@ import os
 import logging
 import sys
 
-logging.basicConfig(stream=sys.stdout,level=logging.INFO)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger('[send_queue]')
 
 
 class MyListener(stomp.ConnectionListener):
     def __init__(self, conn):
         self.conn = conn
-        self.status=True
-        self.message=''
+        self.status = True
+        self.message = ''
 
     def on_error(self, headers, message):
-         logger.error('received an error \n%s' % message)
-         self.status=False
-         self.message=message
-         
+        logger.error('received an error \n%s' % message)
+        self.status = False
+        self.message = message
+
     def on_message(self, headers, message):
-         logger.info('received a message "%s"' % message)
+        logger.info('received a message "%s"' % message)
+
 
 def send_message(resdoc, args, stomp_mversion):
 
@@ -35,14 +36,15 @@ def send_message(resdoc, args, stomp_mversion):
         ssl_flag = True
         logger.info("AMQ SSL: certificate based authentication")
     elif args.username != '' and args.password != '':
-        ssl_flag=False
+        ssl_flag = False
         logger.info("AMQ Plain: user-password based authentication")
     else:
-        raise IOError("The input arguments do not include a valid pair of authentication (certificate, key) or (user,password)")
+        raise IOError(
+            "The input arguments do not include a valid pair of authentication (certificate, key) or (user,password)")
 
-    conn = stomp.Connection(host_and_ports=[(args.server, int(args.port))], use_ssl=ssl_flag, \
-        ssl_key_file=args.key_file, ssl_cert_file=args.cert_file, ssl_version=3)
-    
+    conn = stomp.Connection(host_and_ports=[(args.server, int(args.port))], use_ssl=ssl_flag,
+                            ssl_key_file=args.key_file, ssl_cert_file=args.cert_file, ssl_version=3)
+
     mylistener = MyListener(conn)
     conn.set_listener('mylistener', mylistener)
 
@@ -52,41 +54,49 @@ def send_message(resdoc, args, stomp_mversion):
     else:
         conn.connect(login=args.username, passcode=args.password, wait=True)
 
-    time.sleep(5)  #This nees to stay before the check of the status, in order to get it
+    # This nees to stay before the check of the status, in order to get it
+    time.sleep(5)
 
     if stomp_mversion == 3:
         conn.send(resdoc, destination=args.name)
     else:
         conn.send(body=resdoc, destination=args.name)
 
-    time.sleep(5)  #This nees to stay before the check of the status, in order to get it
+    # This nees to stay before the check of the status, in order to get it
+    time.sleep(5)
 
     if conn.get_listener('mylistener').status == False:
-        raise Exception("Error occurred %s" % conn.get_listener('mylistener').message)
+        raise Exception("Error occurred %s" %
+                        conn.get_listener('mylistener').message)
     conn.disconnect()
 
 
 if __name__ == '__main__':
     stomp_mversion = stomp.__version__[0]
 
-    
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port"     ,required=True, default='', help="Queue port")
-    parser.add_argument("-s", "--server"   ,required=True, default='', help="Queue host")
-    parser.add_argument("-u", "--username" ,nargs='?', default='', help="Queue username")
-    parser.add_argument("-w", "--password" ,nargs='?', default='', help="Queue password")
-    parser.add_argument("-n", "--name"     ,required=True, default='', help="Queue name")
-    parser.add_argument("-k", "--key_file" ,nargs='?', default='', help="AMQ authentication key")
-    parser.add_argument("-c", "--cert_file",nargs='?', default='', help="AMQ authentication certificate")
-    parser.add_argument("-f", "--file"     ,required=True, help="File to send")
+    parser.add_argument("-p", "--port", required=True,
+                        default='', help="Queue port")
+    parser.add_argument("-s", "--server", required=True,
+                        default='', help="Queue host")
+    parser.add_argument("-u", "--username", nargs='?',
+                        default='', help="Queue username")
+    parser.add_argument("-w", "--password", nargs='?',
+                        default='', help="Queue password")
+    parser.add_argument("-n", "--name", required=True,
+                        default='', help="Queue name")
+    parser.add_argument("-k", "--key_file", nargs='?',
+                        default='', help="AMQ authentication key")
+    parser.add_argument("-c", "--cert_file", nargs='?',
+                        default='', help="AMQ authentication certificate")
+    parser.add_argument("-f", "--file", required=True, help="File to send")
     args = parser.parse_args()
 
     if os.path.isfile(args.file) == False:
-        raise IOError("The result input file %s does not exist" %args.file)
+        raise IOError("The result input file %s does not exist" % args.file)
 
-    resdoc = open(args.file,'r').read()
+    resdoc = open(args.file, 'r').read()
 
     logger.info("Sending results to AMQ topic")
     send_message(resdoc, args, stomp_mversion)
     logger.info("Results sent to AMQ topic")
-    
