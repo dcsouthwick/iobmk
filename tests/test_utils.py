41926d447f049cf4fdbc3b57ddfa80184c68224b
Miguel F. Medeiros <miguel.fontes.medeiros@cern.ch>
2020-11-25 14:08:33 +0100

    Increase test coverage for utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,8 +10,158 @@ import difflib
 import json
 import unittest
 from hepbenchmarksuite import utils
+import os
 import sys
 import pytest
+import tempfile
+import tarfile
+import yaml
+import pprint
+from schema import Schema, And, Use, Optional, Or
+
+
+def test_prepare_metadata():
+    """ Test the preparation of metadata."""
+
+    try:
+        with open("hepbenchmarksuite/config/benchmarks.yml", 'r') as cfg_file:
+            sample_config = yaml.full_load(cfg_file)
+
+    except FileNotFoundError:
+        print("Failed to load configuration file.")
+        sys.exit(1)
+
+
+    # Set mp_num since by default is commented
+    sample_config['global']['mp_num']=2
+
+    extra ={
+        'start_time': '',
+        'end_time'  : '',
+    }
+
+    # Generate metadata
+    meta_json = utils.prepare_metadata(sample_config, extra=extra)
+
+    # Drop HW and SW metadata keys sinc this json structure
+    # is already covered in another test hw_metadata
+    del meta_json['host']['HW']
+    del meta_json['host']['SW']
+    print(meta_json)
+
+    # Define suite metadata schema
+    metadata_schema = Schema(
+                        {
+                          str : str,
+                         "suite"         : { str : str,
+                                            "benchmark_version": { str : str },
+                                            "flags"  : { str : str,
+                                                        "mp_num" : int
+                                                       }
+                                           },
+                         "host"          : { str : str,
+                                            "tags"  : { str : str },
+                                           },
+                        },
+                        )
+
+    # Perform assertion test
+    metadata_schema.validate(meta_json)
+
+
+def test_convert_tags_to_json():
+    """ Test convert tags string to json."""
+
+    valid_dict = {"key1" : "value1" , "key2": "value2"}
+
+    assert utils.convert_tags_to_json('{"key1" : "value1" , "key2": "value2"}') == valid_dict
+    assert utils.convert_tags_to_json('{"key1" : "incomplete}') == {}
+
+
+@pytest.mark.parametrize('cmd_str', ["cat /proc/minfo | grep MemTotal", "lxcpu"])
+def test_exec_cmd_fail(cmd_str):
+    """ Test exec for failures."""
+
+    result, return_code = utils.exec_cmd(cmd_str)
+
+    print(result, return_code)
+
+    assert return_code != 0
+
+
+@pytest.mark.parametrize('cmd_str', ["cat /proc/meminfo | grep MemTotal", "uptime"])
+def test_exec_cmd_success(cmd_str):
+    """ Test exec for success. """
+
+    result, return_code = utils.exec_cmd(cmd_str)
+
+    print(result, return_code)
+
+    assert return_code == 0
+
+
+def test_bench_versions():
+    """ Test parsing of benchmark versions. """
+
+    try:
+        with open("hepbenchmarksuite/config/benchmarks.yml", 'r') as cfg_file:
+            sample_config = yaml.full_load(cfg_file)
+
+    except FileNotFoundError:
+        print("Failed to load configuration file.")
+        sys.exit(1)
+
+    # Override benchmark list
+    sample_config['global']['benchmarks'] = ['db12', 'hs06_32', 'hepscore', 'spec2017', 'newbench']
+
+    # Valid benchmark version that should be printed
+    valid_version_output = {
+        "db12"     : "v0.1",
+        "hepscore" : "v1.0rc9",
+        "hs06_32"  : "v1.0",
+        "spec2017" : "v1.0",
+        "newbench" : "not_available",
+
+    }
+    print(utils.bench_versions(sample_config))
+
+    assert utils.bench_versions(sample_config) == valid_version_output
+
+
+def test_export_creation():
+    """Test the export directory from utils."""
+
+    # File extensions
+    file_extensions = ['.json', '.yaml', '.log', '.txt', '.ini', '.logg']
+
+    # Create a temporary directory
+    # It will cleanup automatically after context end
+    with tempfile.TemporaryDirectory() as tmp_dir_path:
+        print('Created temporary directory:', tmp_dir_path)
+
+        # Create temporary files
+        for ext in file_extensions:
+            fd, path = tempfile.mkstemp(suffix=ext, dir=tmp_dir_path)
+            print(fd, path)
+
+        # Check if compressed file was created
+        assert utils.export(tmp_dir_path, 'compressed_file.tgz') == 0
+
+
+def test_export_valid():
+    """Test if the contents of exported compressed file are valid."""
+
+    with tarfile.open('compressed_file.tgz', 'r:gz') as archive:
+        print(archive.list())
+        print(archive.getnames())
+
+        # It should only contain two files in this compressed file: json, log
+        # better logic can be added in the future if needed
+        assert len(archive.getnames()) == 2
+
+    # Clean-up
+    os.remove("compressed_file.tgz")
+
 
 def test_print_results():
     """Test the print results from utils."""
@@ -47,9 +197,18 @@ def test_failed_download(url):
     """Test the download failure."""
     assert utils.download_file(url, "test") == 1
 
-# TODO once a publicly available hepscore url is published
-#def test_success_download():
 
+@pytest.mark.parametrize('url', ["https://gitlab.cern.ch/hep-benchmarks/hep-benchmark-suite/-/raw/qa-v2.0/README.md",
+                                 "https://gitlab.cern.ch/hep-benchmarks/hep-score/-/raw/master/README.md"])
+def test_success_download(url):
+    """Test the download success."""
+    assert utils.download_file(url, "downloaded_README.md") == 0
+    assert os.path.isfile("downloaded_README.md") == 1
+
+
+def test_metadata_version():
+    """Test metadata version."""
+    assert utils.get_version() == 'v2.1-dev'
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
