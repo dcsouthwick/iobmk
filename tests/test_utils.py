fca141b25546d880bbad2e6686566b46e2eb5495
Miguel Fontes Medeiros <miguel.fontes.medeiros@cern.ch>
2021-03-04 13:26:16 +0000

    BMK-709 Update hepscore rc version

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -124,7 +124,7 @@ def test_bench_versions():
     # Valid benchmark version that should be printed
     valid_version_output = {
         "db12"     : "v0.1",
-        "hepscore" : "v1.0rc10",
+        "hepscore" : "v1.0rc12",
         "hs06"     : "v2.0",
         "spec2017" : "v2.0",
         "newbench" : "not_available",
